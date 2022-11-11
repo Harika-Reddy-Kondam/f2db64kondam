3c81634c267bc7ab14a4fec494075fa5bc479a20
Harika-Reddy-Kondam <69505699+Harika-Reddy-Kondam@users.noreply.github.com>
2022-11-10 23:53:56 -0600

    Added changes in controller novel

--- controllers/novel.js
+++ controllers/novel.js
@@ -1,6 +1,5 @@
 var novel = require('../models/novel'); 
  
-// List of all novels 
 // List of all novel 
 exports.novel_list = async function(req, res) { 
     try{ 
@@ -14,9 +13,16 @@ exports.novel_list = async function(req, res) {
 }; 
  
 // for a specific novel. 
-exports.novel_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: novel detail: ' + req.params.id); 
-}; 
+exports.novel_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await novel.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};  
  
 // Handle novel create on POST.  
 exports.novel_create_post = async function(req, res) { 
@@ -25,9 +31,9 @@ exports.novel_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    // {"novelName":"goat", "novelCost":12, "novelAuthor":"large"} 
     document.novelName = req.body.novelName; 
-    document.novelCost = req.body.novelCost; 
+    document.novelnovelCost = req.body.novelnovelCost; 
     document.novelAuthor = req.body.novelAuthor; 
     try{ 
         let result = await document.save(); 
@@ -45,10 +51,25 @@ exports.novel_delete = function(req, res) {
 }; 
  
 // Handle novel update form on PUT. 
-exports.novel_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: novel update PUT' + req.params.id); 
-}; 
-
+exports.novel_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+    ${JSON.stringify(req.body)}`) 
+        try { 
+            let toUpdate = await novel.findById( req.params.id) 
+            // Do updates of properties 
+            if(req.body.novelName)  
+                   toUpdate.novelName = req.body.novelName; 
+            if(req.body.novelCost) toUpdate.novelCost = req.body.novelCost; 
+            if(req.body.novelAuthor) toUpdate.novelAuthor = req.body.novelAuthor; 
+            let result = await toUpdate.save(); 
+            console.log("Sucess " + result) 
+            res.send(result) 
+        } catch (err) { 
+            res.status(500) 
+            res.send(`{"error": ${err}: Update for id ${req.params.id} 
+    failed`); 
+        } 
+    }; 
 // VIEWS 
 // Handle a show all view 
 exports.novel_view_all_Page = async function(req, res) { 
