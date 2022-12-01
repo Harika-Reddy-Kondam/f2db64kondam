1471ae1aa3a95b92b8847384180647c2a59b8453
Harika-Reddy-Kondam <69505699+Harika-Reddy-Kondam@users.noreply.github.com>
2022-11-30 20:59:14 -0600

    Added changes for create

--- routes/novel.js
+++ routes/novel.js
@@ -1,6 +1,15 @@
 var express = require('express');
 const novel_controlers= require('../controllers/novel'); 
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 /* GET home page. */
 
@@ -9,11 +18,11 @@ router.get('/', novel_controlers.novel_view_all_Page);
 
 router.get('/detail', novel_controlers.novel_view_one_Page); 
 
-router.get('/create', novel_controlers.novel_create_Page); 
+router.get('/create',secured, novel_controlers.novel_create_Page); 
 
-router.get('/update', novel_controlers.novel_update_Page);
+router.get('/update',secured, novel_controlers.novel_update_Page);
 
-router.get('/delete', novel_controlers.novel_delete_Page);
+router.get('/delete',secured, novel_controlers.novel_delete_Page);
 
 
 module.exports = router;
